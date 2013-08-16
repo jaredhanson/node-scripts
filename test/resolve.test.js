ed3df21af1428bab6dbf72b7bf7b32d5a5f7e626
Jared Hanson <jaredhanson@gmail.com>
2013-08-16 14:25:56 -0700

    Test cases for resolve.

---
+++ test/resolve.test.js
@@ -0,0 +1,47 @@
+var resolve = require('..').resolve;
+
+describe('resolve', function() {
+    
+  describe('with default extensions', function() {
+    it('should resolve to .js', function() {
+      expect(resolve(__dirname + '/data/js/hello')).to.equal(__dirname + '/data/js/hello.js');
+    });
+    
+    it('should resolve to .js if CoffeeScript is not supported', function() {
+      expect(resolve(__dirname + '/data/coffee/hello')).to.equal(__dirname + '/data/coffee/hello.js');
+    });
+  });
+  
+  describe('with .coffee extension', function() {
+    it('should resolve to .coffee', function() {
+      expect(resolve(__dirname + '/data/coffee/hello', '.coffee')).to.equal(__dirname + '/data/coffee/hello.coffee');
+    });
+    
+    it('should resolve to .coffee using extension without dot prefix', function() {
+      expect(resolve(__dirname + '/data/coffee/hello', 'coffee')).to.equal(__dirname + '/data/coffee/hello.coffee');
+    });
+    
+    it('should resolve to .js if .coffee file not available', function() {
+      expect(resolve(__dirname + '/data/js/hello'), '.coffee').to.equal(__dirname + '/data/js/hello.js');
+    });
+  });
+  
+  describe('with .js preferred over .coffee extension', function() {
+    it('should resolve to .js if .coffee file not available', function() {
+      expect(resolve(__dirname + '/data/js/hello'), ['.js', '.coffee']).to.equal(__dirname + '/data/js/hello.js');
+    });
+    
+    it('should resolve to .coffee if .js file not available', function() {
+      expect(resolve(__dirname + '/data/coffee/hello', ['.js', '.coffee'])).to.equal(__dirname + '/data/coffee/hello.coffee');
+    });
+    
+    it('should resolve to .js if both .js and .coffee are available', function() {
+      expect(resolve(__dirname + '/data/js-coffee/hello', ['.js', '.coffee'])).to.equal(__dirname + '/data/js-coffee/hello.js');
+    });
+  });
+    
+  it('should default to .js for non-existent file', function() {
+    expect(resolve('foo')).to.equal('foo.js');
+  });
+  
+});
